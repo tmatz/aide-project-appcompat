2cd7988850f340cf6ead43ee9d6bc6e1292ad9a8
tmatz <matsu@users.sf.net>
2019-06-22 15:58:43 +0900

    initial

---
+++ app/src/main/java/io/github/tmatz/myapp/MainActivity.java
@@ -0,0 +1,14 @@
+package io.github.tmatz.myapp;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
